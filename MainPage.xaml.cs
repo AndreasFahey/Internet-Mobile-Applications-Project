37cdf6bdc7356f6da22593a9f76b5cdaef835944
AndreasFahey <34457372+AndreasFahey@users.noreply.github.com>
2017-12-12 22:02:48 +0000

    Add files via upload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -142,9 +142,96 @@ namespace MyLittleCalculator
             AddOperationToResult(Operation.DIV);
         }
 
+        #region Equal
+        private class Operand
+        {
+            public Operation operation = Operation.NUMBER;
+            public double value = 0;
+            public Operand left = null;
+            public Operand right = null;
+        }
+        private Operand BuildTreeOperand()
+        {
+            Operand tree = null;
+            string expression = result.Text;
+            if(!char.IsNumber(expression.Last()))
+            {
+                expression = expression.Substring(0, expression.Length - 1);
+            }
+            string numberStr = string.Empty;
+            foreach (char c in expression.ToCharArray())
+            {
+                if (char.IsNumber(c) || c == '-' || numberStr == string.Empty && c == '=')
+                {
+                    numberStr += c;
+                }
+                else
+                {
+                    AddOperandToTree(ref tree, new Operand() { value = double.Parse(numberStr) });
+                    numberStr = string.Empty;
+                    Operation op = Operation.MINUS;
+                    switch (c)
+                    {
+                        case '-': op = Operation.MINUS; break;
+                        case '+': op = Operation.PLUS; break;
+                        case '/': op = Operation.DIV; break;
+                        case 'x': op = Operation.TIMES; break;
+                    }
+                    AddOperandToTree(ref tree, new Operand() { operation = op });
+                }
+            }
+            AddOperandToTree(ref tree, new Operand { value = double.Parse(numberStr) });
+
+            return tree;
+        }
+        private void AddOperandToTree(ref Operand tree, Operand elem)
+        {
+            if (tree == null)
+            {
+                tree = elem;
+            }
+            else
+            {
+                if (elem.operation < tree.operation)
+                {
+                    Operand auxTree = tree;
+                    tree = elem;
+                    elem.left = auxTree;
+                }
+                else
+                {
+                    AddOperandToTree(ref tree.right, elem);
+                }
+            }
+        }
+
+        private double Calc(Operand tree)
+        {
+            if (tree.left == null && tree.right == null)
+            {
+                return tree.value;
+            }
+            else
+            {
+                double subResult = 0;
+                switch (tree.operation)
+                {
+                    case Operation.MINUS: subResult = Calc(tree.left) - Calc(tree.right); break;
+                    case Operation.PLUS: subResult = Calc(tree.left) + Calc(tree.right); break;
+                    case Operation.DIV: subResult = Calc(tree.left) / Calc(tree.right); break;
+                    case Operation.TIMES: subResult = Calc(tree.left) * Calc(tree.right); break;
+                }
+                return subResult;
+            }
+        }
         private void buttonEquals_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrEmpty(result.Text)) return;
+            Operand tree = BuildTreeOperand();
 
+            double value = Calc(tree);
+            result.Text = value.ToString();
         }
+        #endregion
     }
 }
